6f9de3181f8e0b46334146a724f0be02656d83ad
Ujas Vaghani <ujasvaghani@gmail.com>
2023-05-03 10:30:03 -0400

    updated files 5/3/2023

--- dj/dj_app/root/dep_des.py
+++ dj/dj_app/root/dep_des.py
@@ -1,3 +1,18 @@
 import pickle
+import requests
+from bs4 import BeautifulSoup as bs4
 
+class Pull_dep_des:
+    def __init__(self) -> None:
+        pass
+    
+    def pull_flights(self):
+        
+        # add url
+        response = requests.get('url')
+        soup = bs4(response.content, 'html.parser')
 
+        departure = soup('departure')
+        arrival = soup('arrival')
+
+        
