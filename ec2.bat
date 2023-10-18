82a4c3b83d93d3e11348aa58fc51ef97e9094c42
ujazishere <ujasvaghani@gmail.com>
2023-10-17 20:48:01 -0400

    WSGI Degbug, Automation and source update

--- ec2.bat
+++ ec2.bat
@@ -1,4 +1,4 @@
 @echo off
 
 REM stash, pull, Activate the virtual environment; change directory then runserver on the web
-cmd /k "git stash && git pull && c_venv\scripts\activate && cd dj && python manage.py runserver 0.0.0.0:8000"
+cmd /k "git stash && git pull && git stash apply && c_venv\scripts\activate && cd dj && python manage.py runserver 0.0.0.0:8000"
