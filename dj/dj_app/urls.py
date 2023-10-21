c185f5692b3f9777ec7d3eaa5f30dd14ad49d6fa
ismailsakhani <ismailsakhani879@gmail.com>
2023-10-22 01:39:58 +0530

    changes in clock and report page (report page path needs fix in urls.py

--- dj/dj_app/urls.py
+++ dj/dj_app/urls.py
@@ -14,7 +14,9 @@ urlpatterns = [
     path('gate_check', views.gate_check, name = 'gate_check'), #gate_check page.
     path('flight_lookup', views.flight_lookup, name = 'flight_lookup'), #flight_lookup page.
     path('weather', views.weather, name = 'weather'), #weather page. 
-    path('guide', views.guide, name = 'guide'), #guide page.  
+    path('guide', views.guide, name = 'guide'), #guide page. 
+    path('report', views.report_an_issue, name = 'report'), #report page 
  
+
 ]
 
