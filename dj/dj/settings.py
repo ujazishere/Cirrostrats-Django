2667d97cfdb55c677954fd96401107e3e823a608
ismailsakhani <ismailsakhani879@gmail.com>
2023-10-31 12:38:52 +0530

    settings.py

--- dj/dj/settings.py
+++ dj/dj/settings.py
@@ -16,6 +16,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CSRF_TRUSTED_ORIGINS = ['https://cirrostrats.us']
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
