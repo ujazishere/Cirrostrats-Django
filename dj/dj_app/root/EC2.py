5d1f856a6efa3509a8a0e289b819146d9bb90b02
ujazishere <ujasvaghani@gmail.com>
2024-02-14 16:18:21 -0500

    Minor Fixes

--- dj/dj_app/root/EC2.py
+++ dj/dj_app/root/EC2.py
@@ -1,1 +1,11 @@
 EC2_location = "Local Server"
+
+run_lengthy_scrape = True
+bypass_fa = True
+
+
+public_email = 'publicuj@gmail.com'
+smtp_password = "dsxi rywz jmxn qwiz"
+
+personal_email = 'ujasvaghani@gmail.com'
+ismail_email = 'ismailsakhani879@gmail.comm'
