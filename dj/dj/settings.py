8b1f1effbdb55cee1ef132f746a1835e9d5dbe73
ismailsakhani <ismailsakhani879@gmail.com>
2024-03-01 19:37:49 +0530

    csrf search

--- dj/dj/settings.py
+++ dj/dj/settings.py
@@ -16,8 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-CSRF_TRUSTED_ORIGINS = ['https://cirrostrats.us']
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
