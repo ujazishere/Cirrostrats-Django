e0312e2c3c166a798f360d0604a7e9f6a16ae702
ismailsakhani <ismailsakhani879@gmail.com>
2024-03-20 15:58:17 +0530

    random text changes

--- dj/dj_app/static/home.js
+++ dj/dj_app/static/home.js
@@ -15,51 +15,24 @@ document
   });
 
 
+  const scrollingTexts = document.querySelectorAll('.scrolling-text');
+  let currentTextIndex = 0;
 
+  function rotateText() {
+      const currentText = scrollingTexts[currentTextIndex];
+      const nextTextIndex = (currentTextIndex + 1) % scrollingTexts.length;
+      const nextText = scrollingTexts[nextTextIndex];
 
-  window.addEventListener("load", function () {
-    // Array of h3 tag contents
-    const h3Contents = [
-      "Want to know if your gate is occupied in Newark? Look up the gate. For example: 71x",
-      "Hit the live map to see the HD weather radar",
-      "Want to know the weather at a particular airport? Look it up the identifier. E.g: KEWR",
-    ];
+      currentText.classList.remove('active');
+      currentText.classList.add('hidden');
 
-    // Get the h3 element
-    const randomH3 = document.getElementById("randomH3");
+      nextText.classList.remove('hidden');
+      nextText.classList.add('active');
 
-    // Get the last displayed index from local storage or default to 0
-    let index = parseInt(localStorage.getItem("lastDisplayedIndex")) || 0;
+      currentTextIndex = nextTextIndex;
+  }
 
-    function displayNextText() {
-      // Set the content of the h3 element to the next content in the array
-      randomH3.innerHTML = `
-        <svg class="bi me-2" width="16" height="16" fill="currentColor">
-          <use xlink:href="#info-fill"/>
-        </svg>
-        ${h3Contents[index]}
-      `;
-
-      // Add the animation class to make the h3 visible with animation
-      randomH3.classList.add("visible");
-
-      // Increment the index for next content
-      index++;
-
-      // If reached the end of content, reset index to 0
-      if (index >= h3Contents.length) {
-        index = 0;
-      }
-
-      // Store the index in local storage
-      localStorage.setItem("lastDisplayedIndex", index.toString());
-    }
-
-    // Initial display
-    displayNextText();
-
-
-  });
+  setInterval(rotateText, 3000); // Change text every 3 seconds
 
 
 
